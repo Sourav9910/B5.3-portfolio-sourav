a2184c93cb0acc86708450ba3a811308f70613ee
Sourav Som <souravsom.vawsum@gmail.com>
2026-01-08 22:07:17 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 
-  // When a nav-link is clicked
-  document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', function () {
-      // Remove active class from all nav-links
-      document.querySelectorAll('.nav-link').forEach(el => el.classList.remove('active'));
-      // Add active to the clicked one
-      this.classList.add('active');
-    });
+// When a nav-link is clicked
+document.querySelectorAll('.nav-link').forEach(link => {
+  link.addEventListener('click', function () {
+    // Remove active class from all nav-links
+    document.querySelectorAll('.nav-link').forEach(el => el.classList.remove('active'));
+    // Add active to the clicked one
+    this.classList.add('active');
   });
+});
 
 
-  // dark theme and light theme
+// dark theme and light theme
 
- 
+
 const toggleBtn = document.getElementById('themeToggle');
 const body = document.body;
 
@@ -27,5 +27,18 @@ toggleBtn.addEventListener('click', () => {
     : '<i class="fa-solid fa-moon"></i>';
 });
 
+// about section tab-links
+let tablinks = document.querySelectorAll('.tab-links');
+let tabcontents = document.querySelectorAll('.tab-contents');
 
+function opentab(tabName) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabName).classList.add("active-tab");
+}
 
